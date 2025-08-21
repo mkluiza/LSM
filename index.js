3e875215745b66feacd70b98fcba2a26528b3024
Macbook <macbook@Macbooks-Luiza-Air.local>
2025-08-21 21:56:22 +0300

    up

--- index.js
+++ index.js
@@ -20,3 +20,12 @@ app.listen(PORT, () => {
 // $ ssh { hébergement_id } @git.{ datacenter_id }.gpaas.net 'deploy {repository}.git
 // $ ssh eee0a83c - 7ce1 - 11f0 - bd2b-00163eada87b @git.sd6.gpaas.net 'deploy default.git'
 //  ssh eee0a83c-7ce1-11f0-bd2b-00163eada87b@git.sd6.gpaas.net 'clean default.git'
+
+// git remote add gandi ssh://git@your_git_server/your_app_name.git
+
+// git_server = ssh://git@git.sd6.gpaas.net/vhosts/default/
+// app_name = 
+// user_name_gandi_sftp = eee0a83c-7ce1-11f0-bd2b-00163eada87b
+// host_name_gandi = git.sd6.gpaas.net / sftp.sd6.gpaas.net / ssh.sd6.gpaas.net
+// psswd = Tdw8bky9aP7dZYV
+//  control_panel_user_name+ = 11319408
